bf2d4aa28ca407d9d6f5f8750f5804a5bf68f022
baixue <bx2651@foxmail.com>
2020-08-05 23:41:01 +0800

    文件位置修改/对象

--- javaScript/test.js
+++ javaScript/test.js
@@ -1,1 +1,15 @@
-console.log(JSON.stringify(['aaaa']))
+function Person(name,age){
+  this.name = name
+  this.age = age
+  if(typeof this.sayName != 'function'){
+    Person.prototype.sayName=function(){
+      console.log("say name",this.name)
+    }
+  }
+}
+
+
+const person = new Person()
+console.log(person) //[ 'Jack', 'Lily', 'Helen' ]
+
+
